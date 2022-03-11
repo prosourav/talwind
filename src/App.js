31a53ec2eeed7c5a574861d837715cbfaa0bb9f9
prosourav <dev.souravsaha@gmail.com>
2022-03-12 01:21:30 +0530

    started with taliwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import image from './38.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <div className="p-10 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 flex-col">
+  <div className="shrink-0  ">
+    <img className="h-16 w-46 " src={image} alt="ChitChat Logo" />
+  </div>
+  <div>
+    <div className="text-xl font-medium text-black">ChitChat</div>
+    <p className="text-slate-500">You have a new message!</p>
+  </div>
+</div>
+  
     </div>
   );
 }
